48beea7793fb375e0e816b47f0fbd22a0368d206
BuildTools <unconfigured@null.spigotmc.org>
2021-02-24 21:55:50 +0200

    Identation fixes

--- api/routes/task2Routes.js
+++ api/routes/task2Routes.js
@@ -1,9 +1,8 @@
 'use strict';
+
 module.exports = function(app) {
-  var bpi = require('../controllers/task2Controller');
-
-  // todoList Routes
-
-  app.route('/api/getPriceHistory/:startDate/:endDate')
-    .get(bpi.execute);
+    var bpi = require('../controllers/task2Controller');
+    
+    app.route('/api/getPriceHistory/:startDate/:endDate')
+        .get(bpi.execute);
 };
