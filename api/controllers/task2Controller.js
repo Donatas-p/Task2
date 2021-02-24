8cc307073f1161a8f2d12ef77222a01af3f16a3c
BuildTools <unconfigured@null.spigotmc.org>
2021-02-24 21:52:45 +0200

    fixed some spacing in controller, and fixed fetch bug

--- api/controllers/task2Controller.js
+++ api/controllers/task2Controller.js
@@ -1,14 +1,14 @@
 'use strict';
 
 exports.execute = function(req, res) {
-    var params = req.params;
-    var startDate = params.startDate;
-    var endDate = params.endDate;
+    var reqParams = req.params;
+    var startDate = reqParams.startDate;
+    var endDate = reqParams.endDate;
     const fetch = require('node-fetch');
 
     fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start='+startDate+'&end='+endDate)
         .then(checkStatus)
-        .then(response => res.text())
+        .then(response => response.text())
         .then(data => {
             if (testJson(data) == true) {
                 var dataObj = JSON.parse(data);
@@ -27,7 +27,7 @@ exports.execute = function(req, res) {
         .catch(err => console.error(err));
 
     function checkStatus(response) {
-          if (response.ok) {
+        if (response.ok) {
             return response;
         } else {
             res.json({error: response.statusText});
@@ -37,8 +37,7 @@ exports.execute = function(req, res) {
     function testJson(text) {
         if (typeof text !== "string") { 
             return false;
-        } 
-        try { 
+        } try { 
             JSON.parse(text);
             return true;
         } catch (error) { 
