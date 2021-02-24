cb5b707696039e910d3bc271ecc7c321681eaba2
BuildTools <unconfigured@null.spigotmc.org>
2021-02-25 00:32:13 +0200

    Added Rate Limiter

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express'),
     compression = require('compression'),
+    rateLimit = require("express-rate-limit"),
     app = express(),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser');
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(compression());
+app.use(limiter);
 app.use((req, res, next) => {
     let validIps = ['::1', '127.0.0.1', '78.57.180.189'];
 
