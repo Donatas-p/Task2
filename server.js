2812b953780a7fae8b262aadc04b50b1fd0e3719
BuildTools <unconfigured@null.spigotmc.org>
2021-02-24 21:55:57 +0200

    Added IP Check

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    let validIps = ['::1', '127.0.0.1', '78.57.180.189'];
+
+    if(validIps.includes(req.socket.remoteAddress)){
+        next();
+    } else {
+      const err = new Error("Bad IP: " + req.socket.remoteAddress);
+      next(err);
+    }
+})
+
 var routes = require('./api/routes/task2Routes');
 routes(app);
 
